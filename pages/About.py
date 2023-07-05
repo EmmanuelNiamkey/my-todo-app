a6c2deab7b1d523ae1b0b5583a9bc05c9e3d7f1a
emmanuelniamkey <yfniamkey@gmail.com>
2023-07-05 16:54:08 -0600

    new pages implemented

---
+++ pages/About.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.write("New app by <b>Emmanuel N.<b/>",
+         unsafe_allow_html=True)
+
+st.write("latest version <b>July 4th 2023.<b/>",
+         unsafe_allow_html=True)
